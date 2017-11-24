2d5b79d32c825486928a584ca3517cb6e58e375a
Guy Allard <w.g.allard@lumc.nl>
2017-11-24 13:46:50 +0100

    search for short tandem repeats if specified in locus definition

--- scripts/call_variants.py
+++ scripts/call_variants.py
@@ -2,12 +2,12 @@
 Call variants for allele sequences
 """
 from Bio import SeqIO
-from variant_tools import alignment, variant_calling
+from variant_tools import alignment, variant_calling, str_search
 from collections import OrderedDict
 import json
 import locus_processing
 import yaml
-
+from snakemake import WorkflowError
 
 locus = locus_processing.load_locus_yaml(snakemake.input.locus)
 
@@ -18,8 +18,38 @@ try:
 except (KeyError, IOError):
     start_pos = locus.coordinates.start
 
-# load the reference sequence
-ref_seq = str(SeqIO.read(snakemake.input.reference, "fasta").seq)
+
+def find_and_mask_repeats(query_seq, ref_seq):
+    locus_strs = [str_search.parse_hgvs(s.g_notation) for s in locus.snps if s.tags is not None and 'short_tandem_repeat' in s.tags]
+    strs = set()
+    masked_query = query_seq
+    for repeat in set((s["start"], s["unit"]) for s in locus_strs):
+        
+        # currently, we can only handle str nomenclature that INCLUDES the repeat motif
+        if repeat[1] == "":
+            raise WorkflowError("Cannot parse str notation without reference string")
+        
+        # find all occurrences of the specified repeat
+        found = str_search.str_search_aligned(repeat[1], query_seq)
+        
+        # identify which of the found repeats correspond to the query repeat
+        for f in found:
+            ref_start = len([_ for _ in ref_seq[:f["start"]] if _ != "-"]) + start_pos
+            if ref_start == repeat[0]:
+                strs.add(
+                    "{}_{}{}[{}]".format(
+                        ref_start, 
+                        ref_start + len(f["canonical_unit"]) - 1,
+                        f["canonical_unit"],
+                        f["num_units"]
+                    )
+                )
+                
+                # mask this repeat region in the query sequence
+                masked_query = masked_query[:f["start"]] + "$" * (f["end"] + 1 - f["start"]) + masked_query[f["end"] + 1:]
+    
+    return list(strs), masked_query
+    
 
 # process the alleles
 with open(snakemake.input.alleles, "r") as alleles, \
@@ -28,6 +58,9 @@ with open(snakemake.input.alleles, "r") as alleles, \
 
     # somewhere to put the results
     result_list = []
+    
+    # load the reference sequence
+    ref_seq = str(SeqIO.read(snakemake.input.reference, "fasta").seq)
     
     for allele in SeqIO.parse(alleles, "fasta"):
         # align the allele to the ref
@@ -38,6 +71,10 @@ with open(snakemake.input.alleles, "r") as alleles, \
         print(allele.id, file=alnfile)
         print(alignment.pretty_alignment(aln[0], aln[1], width=100, match="="), file=alnfile)
         print("", file=alnfile)
+        
+        # search and mask repeats in the aligned allele sequence
+        strs, masked_allele = find_and_mask_repeats(aln[1], aln[0])
+        aln = (aln[0], masked_allele)
 
         # calculate similarity
         distance = min(alignment.get_strand_dists(allele_seq, ref_seq))
@@ -48,7 +85,7 @@ with open(snakemake.input.alleles, "r") as alleles, \
         result = OrderedDict()
         result["sequence_id"] = allele.id
         result["identity"] = identity
-        result["variants"] = [str(v) for v in variant_calling.call_variants(aln, start_pos - 1)]
+        result["variants"] = [str(v) for v in variant_calling.call_variants(aln, start_pos - 1)] + strs
         result_list.append(result)
 
     # dump results to json
